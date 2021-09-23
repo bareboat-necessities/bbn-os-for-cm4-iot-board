4424027f332ae8141c0d642faabb1ed677356b34
mgrouch <mgrushinskiy@gmail.com>
2021-09-22 21:56:00 -0400

    fix

--- install-scripts/0-repositories/00-updateRepo.sh
+++ install-scripts/0-repositories/00-updateRepo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-apt-get update  -y -q
-apt-get install -y -q wget gnupg ca-certificates ssl-cert
+apt-get update  -y -q || if [ $? == 100 ]; then true; fi
+apt-get install -y -q wget gnupg ca-certificates ssl-cert || if [ $? == 100 ]; then true; fi
 
 ## Update && Upgrade
-apt-get update  -y -q
-apt-get upgrade -y -q
+apt-get update  -y -q || if [ $? == 100 ]; then true; fi
+apt-get upgrade -y -q || if [ $? == 100 ]; then true; fi
