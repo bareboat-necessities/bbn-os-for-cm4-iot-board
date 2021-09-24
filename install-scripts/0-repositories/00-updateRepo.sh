580e8b6d1ec1cf9e818205a644ca157f40462cfd
mgrouch <mgrushinskiy@gmail.com>
2021-09-24 19:41:20 -0400

    fix

--- install-scripts/0-repositories/00-updateRepo.sh
+++ install-scripts/0-repositories/00-updateRepo.sh
@@ -3,6 +3,3 @@
 apt-get update  -y -q || if [ $? == 100 ]; then true; fi
 apt-get install -y -q wget gnupg ca-certificates ssl-cert || if [ $? == 100 ]; then true; fi
 
-## Update && Upgrade
-apt-get update  -y -q || if [ $? == 100 ]; then true; fi
-apt-get upgrade -y -q || if [ $? == 100 ]; then true; fi
