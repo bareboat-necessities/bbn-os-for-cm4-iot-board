98ab8d0ce88f0ed926453f5c7440d4db100f9fe9
mgrouch <mgrushinskiy@gmail.com>
2021-10-01 10:32:38 -0400

    fan

---
+++ install-scripts/2-baseSystem/03-fan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+CUR_DIR="$(pwd)"
+
+mkdir -p /home/user/fan &&  cd /home/user/fan
+
+git clone https://github.com/neg2led/cm4io-fan.git
+cd cm4io-fan
+chmod 777 install.sh
+./install.sh
+
+cd "$CUR_DIR"
+
+echo "dtoverlay=cm4io-fan,minrpm=500,maxrpm=5000,midtemp=45000,midtemp_hyst=2000,maxtemp=50000,maxtemp_hyst=2000" \
+ >> /boot/config.txt
+
