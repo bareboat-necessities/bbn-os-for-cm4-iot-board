5c159382f6158c5daceb2b7ff519a5a526591a61
mgrouch <mgrushinskiy@gmail.com>
2021-10-01 15:55:30 -0400

    fan

--- install-scripts/2-baseSystem/03-fan.sh
+++ install-scripts/2-baseSystem/03-fan.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
 
-apt-get -y -q install  raspberrypi-kernel-headers:arm64 dkms
-
-CUR_DIR="$(pwd)"
-
-mkdir -p /home/user/fan &&  cd /home/user/fan
-
-git clone https://github.com/neg2led/cm4io-fan.git
-cd cm4io-fan
-chmod 777 install.sh
-./install.sh
-
-cd "$CUR_DIR"
-
-echo "dtoverlay=cm4io-fan,minrpm=500,maxrpm=5000,midtemp=45000,midtemp_hyst=2000,maxtemp=50000,maxtemp_hyst=2000" \
- >> /boot/config.txt
-
+#apt-get -y -q install  raspberrypi-kernel-headers dkms
+#
+#CUR_DIR="$(pwd)"
+#
+#mkdir -p /home/user/fan &&  cd /home/user/fan
+#
+#git clone https://github.com/neg2led/cm4io-fan.git
+#cd cm4io-fan
+#chmod 777 install.sh
+#./install.sh
+#
+#cd "$CUR_DIR"
+#
+#echo "dtoverlay=cm4io-fan,minrpm=500,maxrpm=5000,midtemp=45000,midtemp_hyst=2000,maxtemp=50000,maxtemp_hyst=2000" \
+# >> /boot/config.txt
+#
