36f608bdd6ac502fc0aba7789c60196ad5561b39
mgrouch <mgrushinskiy@gmail.com>
2021-11-27 12:38:05 -0500

    more threads

--- .circleci/publish-cloudsmith.sh
+++ .circleci/publish-cloudsmith.sh
@@ -17,7 +17,7 @@ for pkg_file in cross-build-release/release/*/*.$EXT; do
   mkdir ./tmp
   chmod 755 ./tmp
   cd $zipDir || exit 255
-  xz -z -c -v -8 --threads=2 --memory=1G ${zipName} > ../../../tmp/${zipName}.xz
+  xz -z -c -v -8 --threads=4 --memory=2G ${zipName} > ../../../tmp/${zipName}.xz
   cd ../../..
   cloudsmith push raw $REPO ./tmp/${zipName}.xz --summary "LysMarine built by CircleCi on $(date)" --description "LysMarine BBN build for Waveshare board"
   RESULT=$?
